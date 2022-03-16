e962718bfb0fb0446bd296e4ed02f23c9d1204e8
1de-a <01101001de@gmail.com>
2022-03-16 19:07:22 +0300

    first functions created

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 let kelimeHavuzu = ["sezgi","vakit","müdür","pamuk","demir"];
 
-console.log("Merhaba");
 
 let elements;
 let a;
 let checkChar = 0;
 let checkLine = 0;
+let deneme1;
+let deneme2;
+let deneme3;
+let deneme4;
+let deneme5;
+let deneme6;
+let soru = "";
+let soruArray;
+let deneme1array;
 
 
-console.log(document.getElementById(1).innerHTML )
 
-function write() {
-
-  alert("Lan!")
-
-  
-}
 
  elements = document.getElementsByClassName("key");
 
@@ -33,12 +34,15 @@ if
     
     checkChar = checkChar+1;
     console.log(checkChar);
+    console.log(soru);
     break
 
   }
 
 }
-  
+
+
+
 };
 
 
@@ -48,3 +52,28 @@ for (var i = 0; i < elements.length ; i++) {
     elements[i].addEventListener('click', myFunction, false);
     
 }
+
+
+function enter() {
+
+
+if (soru==="") {soru = kelimeHavuzu[Math.floor(Math.random() * (kelimeHavuzu.length+ 1) )]};
+
+
+console.log(soru);
+
+
+  if (checkChar%5==0) { 
+  
+    deneme1 = document.getElementById(1).innerHTML + document.getElementById(2).innerHTML + document.getElementById(3).innerHTML + document.getElementById(4).innerHTML + document.getElementById(5).innerHTML ;
+    console.log(deneme1)
+  }
+
+ soruArray = soru.split("");
+console.log(soruArray);
+ deneme1array = deneme1.split("");
+console.log(deneme1array);
+
+for(i=0;i<5;i++) {if (soruArray[i]===deneme1array[i]){document.getElementById(i+1).style.backgroundColor="green"}}
+}
+
